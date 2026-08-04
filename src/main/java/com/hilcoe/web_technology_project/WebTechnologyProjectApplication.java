f7a26a26f1d50540cb60cfa5e7d480badf5d0064
Surafel Tadesse Wolde <stadesse9@gmail.com>
2026-08-04 12:35:46 +0300

    first commit

---
+++ src/main/java/com/hilcoe/web_technology_project/WebTechnologyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hilcoe.web_technology_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTechnologyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebTechnologyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hilcoe/web_technology_project/WebTechnologyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilcoe.web_technology_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTechnologyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
