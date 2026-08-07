392962458371f33c8c6eebe0e91e60c1009208ba
Surafel Tadesse Wolde <stadesse9@gmail.com>
2026-08-07 22:53:53 +0300

    entities is being created for the project

---
+++ src/main/java/com/hilcoe/web_technology_project/entity/EventStatus.java
@@ -0,0 +1,9 @@
+package com.hilcoe.web_technology_project.entity;
+
+public enum EventStatus {
+    DRAFT,
+    PUBLISHED,
+    ONGOING,
+    COMPLETED,
+    CANCELLED
+}
---
+++ src/main/java/com/hilcoe/web_technology_project/entity/EventType.java
@@ -0,0 +1,9 @@
+package com.hilcoe.web_technology_project.entity;
+
+public enum EventType {
+    WEDDING,
+    BIRTHDAY_PARTY,
+    CONFERENCE,
+    CONCERT_FESTIVAL,
+    CORPORATE_MEETING
+}
