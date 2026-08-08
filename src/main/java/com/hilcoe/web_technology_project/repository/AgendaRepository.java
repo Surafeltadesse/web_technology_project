354a34188497275ddd48f830c6302b65faaaf62d
Abraham <abrahameshete@Abrahams-MacBook-Air.local>
2026-08-08 16:49:41 +0300

    repository for the project is created

---
+++ src/main/java/com/hilcoe/web_technology_project/repository/AgendaRepository.java
@@ -0,0 +1,14 @@
+package com.hilcoe.web_technology_project.repository;
+
+
+import com.surafel.event_and_guest_managment_system.entity.Agenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AgendaRepository extends JpaRepository<Agenda, Long> {
+    List<Agenda> findByEventIdOrderByStartTimeAsc(Long eventId);
+    void deleteByEventId(Long eventId);
+}
